9c262ec0be48540c0a8310a62c18b9d5e93453e4
OlexaNdrus <abysho666@gmail.com>
2019-04-12 15:12:56 +0300

    ignore

--- django_blogER/settings.py
+++ django_blogER/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,6 +140,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('email_user')
 EMAIL_HOST_PASSWORD = os.environ.get('email_password')
+
+django_heroku.settings(locals())
 
 
 
